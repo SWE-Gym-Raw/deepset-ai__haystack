22bfefee784ee17d421fcc1de5f8c877c081f365
David S. Batista <dsbatista@gmail.com>
2025-01-07 21:50:53 +0100

    fix: adding double \n\n to passages when extracting text from PDF with PDFMiner

--- test/components/converters/test_pdfminer_to_document.py
+++ test/components/converters/test_pdfminer_to_document.py
@@ -5,6 +5,7 @@ import logging
 
 import pytest
 
+from haystack.components.preprocessors import DocumentSplitter
 from haystack.dataclasses import ByteStream
 from haystack.components.converters.pdfminer import PDFMinerToDocument
 
@@ -150,3 +151,38 @@ class TestPDFMinerToDocument:
             results = converter.run(sources=sources)
             assert "PDFMinerToDocument could not extract text from the file" in caplog.text
             assert results["documents"][0].content == ""
+
+    def test_run_split_passage(self, test_files_path):
+        converter = PDFMinerToDocument()
+        sources = [test_files_path / "pdf" / "sample_pdf_2.pdf"]
+        pdf_doc = converter.run(sources=sources)
+        splitter = DocumentSplitter(split_length=1, split_by="passage")
+        docs = splitter.run(pdf_doc["documents"])
+
+        assert len(docs["documents"]) == 22
+        assert docs["documents"][4].content == (
+            """A wiki (/ˈwɪki/ (About this soundlisten) WIK-ee) is a hypertext publication collaboratively \nedited and
+            managed by its own audience directly using a web browser. A typical wiki \ncontains multiple pages for the
+            subjects or scope of the project and may be either open \nto the public or limited to use within an
+            organization for maintaining its internal knowledge \nbase. Wikis are enabled by wiki software, otherwise
+            known as wiki engines. A wiki engine, \nbeing a form of a content management system, diﬀers from other
+            web-based systems \nsuch as blog software, in that the content is created without any deﬁned owner or
+            leader, \nand wikis have little inherent structure, allowing structure to emerge according to the \nneeds
+            of the users.[1] \n\n"""
+        )
+        assert (
+            docs["documents"][5].content
+            == """\nWiki engines usually allow content to be written using a simpliﬁed markup language and \nsometimes
+            edited with the help of a rich-text editor.[2] There are dozens of diﬀerent wiki \nengines in use, both
+            standalone and part of other software, such as bug tracking \nsystems. Some wiki engines are open source,
+            whereas others are proprietary. Some \npermit control over diﬀerent functions (levels of access); for
+            example, editing rights may \npermit changing, adding, or removing material. Others may permit access
+            without \nenforcing access control. Other rules may be imposed to organize content. The online
+             \nencyclopedia project Wikipedia is the most popular wiki-based website, and is one of the \nmost widely
+             viewed sites in the world, having been ranked in the top ten since 2007.[3] \nWikipedia is not a single
+             wiki but rather a collection of hundreds of wikis, with each one \npertaining to a speciﬁc language. In
+             addition to Wikipedia, there are hundreds of \nthousands of other wikis in use, both public and private,
+             including wikis functioning as \nknowledge management resources, notetaking tools, community websites, and
+             intranets. \nThe English-language Wikipedia has the largest collection of articles: as of February 2020,
+             \nit has over 6 million articles. \n\n"""
+        )
